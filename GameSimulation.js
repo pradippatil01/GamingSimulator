67fee676a0c8a89fbfdecd7490080b9be37c5dac
pradippatil01 <mr.pradip01@gmail.com>
2021-01-24 09:31:51 +0530

    Add : winLoss method added for win loss feature

--- GameSimulation.js
+++ GameSimulation.js
@@ -1,3 +1,20 @@
 const STAKE = 100;
 const BET = 1;
+const WIN = 1;
+const LOSS = 0;
+class GameSimulator {
+    winLoss = () => {
+        let betNumber = Math.floor(Math.random() * 2);
+        switch (betNumber) {
+            case 0:
+                console.log('loss 1$');
+                break;
+            case 1:
+                console.log('win 1$');
+                break;
+        }
+    }
+}
+let gameSimulator = new GameSimulator();
 console.log('Welcome to the GAMBLING SIMULATION');
+gameSimulator.winLoss();
