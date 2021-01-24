fd31ba6cf17be5171b14e3d0321687b6140b1707
pradippatil01 <mr.pradip01@gmail.com>
2021-01-24 20:23:19 +0530

    Add : findLuckiestAndUnLuckiestMonth method added to find lucky month and unlucky month

--- GameSimulation.js
+++ GameSimulation.js
@@ -5,17 +5,17 @@ const LOSS = 0;
 const DAILY_LOW_LIMIT = (STAKE / 2);
 const DAILY_HIGH_LIMIT = (STAKE / 2 + STAKE);
 var monthArray = ['Jan', 'feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-var dayRecordData = new Map();
 var monthRecordData = new Map();
 var dayLoss = new Map();
 var dayWon = new Map();
-var daysValueMap = new Map();
+var unluck
 class GameSimulator {
     winLoss = () => {
+        var cash = 0;
         for (let j = 0; j < monthArray.length; j++) {
             var gameRecord = [];
             for (let i = 0; i < 20; i++) {
-                var cash = STAKE;
+                cash = STAKE;
                 while (DAILY_LOW_LIMIT < cash && DAILY_HIGH_LIMIT > cash) {
                     let betNumber = Math.floor(Math.random() * 2);
                     switch (betNumber) {
@@ -33,8 +33,9 @@ class GameSimulator {
             this.monthRecord(gameRecord, monthArray[j])
         }
     }
+
     dailyRecord = (gameData, month) => {
-        console.log(month,"==>")
+        console.log(month, "==>")
         for (let i = 0; i < gameData.length; i++) {
             console.log('Day ', i, ' record ', gameData[i]);
         }
@@ -52,11 +53,31 @@ class GameSimulator {
             console.log(monthArray[i], ' total cash ==> ', monthRecordData.get(monthArray[i]))
         }
     }
+
+    findLuckiestAndUnLuckiestMonth = () => {
+        var maxLoss = monthRecordData.get(monthArray[0]);
+        var index;
+        for (let i = 0; i < monthRecordData.size; i++) {
+            if (maxLoss > monthRecordData.get(monthArray[i])) {
+                maxLoss = monthRecordData.get(monthArray[i]);
+                index = monthArray[i];
+            }
+        }
+        console.log('unlucky month', index, 'with profit', maxLoss);
+        var maxProfit = monthRecordData.get(monthArray[0]);
+        var index;
+        for (let i = 0; i < monthRecordData.size; i++) {
+            if (maxProfit < monthRecordData.get(monthArray[i])) {
+                maxProfit = monthRecordData.get(monthArray[i]);
+                index = monthArray[i];
+            }
+        }
+        console.log('lucky month', index, 'with profit', maxProfit)
+    }
 }
 
 let gameSimulator = new GameSimulator();
 console.log('Welcome to the GAMBLING SIMULATION');
 gameSimulator.winLoss();
 gameSimulator.yearRecord();
-
- 
+gameSimulator.findLuckiestAndUnLuckiestMonth();
