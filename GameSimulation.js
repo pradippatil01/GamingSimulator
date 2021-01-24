ec160de82e957c911a8e85a6af3691ab608baf93
pradippatil01 <mr.pradip01@gmail.com>
2021-01-24 10:44:22 +0530

    Add : dailyRecord method added and store daily profit loss in array and display

--- GameSimulation.js
+++ GameSimulation.js
@@ -4,23 +4,28 @@ const WIN = 1;
 const LOSS = 0;
 DAILY_LOW_LIMIT = (STAKE / 2);
 DAILY_HIGH_LIMIT = (STAKE / 2 + STAKE);
-
+var gameRecord = [];
 class GameSimulator {
     winLoss = () => {
-        var cash = STAKE;
-        while (DAILY_LOW_LIMIT < cash && DAILY_HIGH_LIMIT > cash) {
-            let betNumber = Math.floor(Math.random() * 2);
-            switch (betNumber) {
-                case 0:
-                    console.log('loss 1$');
-                    cash = cash - BET;
-                    break;
-                case 1:
-                    console.log('win 1$');
-                    cash = cash + BET;
-                    break;
+        for (let i = 1; i <= 20; i++) {
+            var cash = STAKE;
+            while (DAILY_LOW_LIMIT < cash && DAILY_HIGH_LIMIT > cash) {
+                let betNumber = Math.floor(Math.random() * 2);
+                switch (betNumber) {
+                    case 0:
+                        cash = cash - BET;
+                        break;
+                    case 1:
+                        cash = cash + BET;
+                        break;
+                }
             }
-            console.log('cash in hand ', cash);
+            gameRecord[i] = cash;
+        }
+    }
+    dailyRecord = () => {
+        for (let i = 1; i <= 20; i++) {
+            console.log('Day ',i,' record ', gameRecord[i]);
         }
     }
 }
@@ -28,3 +33,4 @@ class GameSimulator {
 let gameSimulator = new GameSimulator();
 console.log('Welcome to the GAMBLING SIMULATION');
 gameSimulator.winLoss();
+gameSimulator.dailRecord();
